dfdfc42694e39ea263c8b7b37652d2012de81bd6
mingzhouChe <cmz1754@gmail.com>
2022-03-27 21:06:45 +0800

    Update test_create_env.py

--- test_create_env.py
+++ test_create_env.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 22 15:43:08 2022
-
-@author: mingz
-"""
 
 import rlcard
 
@@ -12,10 +6,6 @@ env = rlcard.make('limit-holdem', config={
     ,'allow_step_back': True
     })
 
-env = rlcard.make('no-limit-holdem', config={
-    'seed': 123
-    ,'allow_step_back': True
-    })
 
 from rlcard.utils import get_device, set_seed, tournament, reorganize, Logger, plot_curve
 from rlcard.agents import RandomAgent
@@ -28,8 +18,6 @@ agent = DQNAgent(num_actions=env.num_actions,
                  device=device)
 
 agents = [agent]
-
-agents = []
 for _ in range(0, env.num_players):
     agents.append(RandomAgent(num_actions=env.num_actions))
 env.set_agents(agents)
@@ -41,64 +29,7 @@ for i in range(5000):
     trajectories = reorganize(trajectories, payoffs)
     all_trajectories.extend(trajectories)
 
-
-# Feed transitions into agent memory, and train the agent
-# Here, we assume that DQN always plays the first position
-# and the other players play randomly (if any)
-for ts in trajectories[0]:
-    agent.feed(ts)
-
 tournament(env, 20)
 tournament(env, 2000)
 tournament(env, 5000)
-
-len(trajectories)
-
-trajectories[0][0]
-trajectories[1][0]
-
-trajectories[0][1]
-trajectories[1][1]
-
-trajectories[0][2]
-trajectories[1][2]
-
-payoffs
-
-
-
-trajectories_2 = reorganize(trajectories, payoffs)
-
-len(trajectories_2)
-trajectories_2[0][0]
-trajectories_2[1][0]
-trajectories_2[1][0]
-trajectories_2[1][1]
-
-
-# one example
-state, player_id = env.reset()
-action = env.agents[player_id].step(state)
-# Environment steps
-next_state, next_player_id = env.step(action, env.agents[player_id].use_raw)
-# another player take action
-action = env.agents[player_id].step(state)
-next_state, next_player_id = env.step(action, env.agents[player_id].use_raw)
-
-action = 1#env.agents[player_id].step(state)
-next_state, next_player_id = env.step(action, env.agents[player_id].use_raw)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tournament(env, 10000)
